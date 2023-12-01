356adc406b25323ddd8021e16531b8a4936b25d1
abraham-1 <esp19005@byui.edu>
2023-12-01 07:33:44 -0600

    Update baseURL

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // Implement the axios configuration
 export default axios.create({
-    baseURL: 'https://abrahameg-movies.onrender.com'
+    baseURL: 'https://disneymoviesapi-xwpfeo27.b4a.run/'
 });
